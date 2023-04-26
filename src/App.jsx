08c2826bb9317593c4afe9bb14adee5431407dc2
Ahsan Ullah Khan <ahsanullahkhan0369@gmail.com>
2023-04-26 12:00:36 +0500

    Setup Completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="bg-black text-red text-white text-4x font-bold">
+        Welcome to Our E-Commerce Store
+      </h1>
+    </>
+  );
+};
+
+export default App;
